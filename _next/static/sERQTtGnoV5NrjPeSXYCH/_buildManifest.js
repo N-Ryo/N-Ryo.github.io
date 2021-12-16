531317c88a163ddff289a7ab7cb1150db813cd50
N-Ryo <N-Ryo@users.noreply.github.com>
2021-12-16 15:14:11 +0000

    deploy: e0e10c1316ffdb020ec4aa782ac0b7c26e9ca328

---
+++ _next/static/sERQTtGnoV5NrjPeSXYCH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-05e11117a839dca2.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],"/linkSample":["static/chunks/pages/linkSample-e31986a628cb2d3f.js"],sortedPages:["/","/_app","/_error","/linkSample"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
